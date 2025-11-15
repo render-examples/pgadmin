921ddd8792f864cfb0cf712f5829397e9b59fe38
Younus Kawa Mohammed <77130132+Yuri1232@users.noreply.github.com>
2025-11-15 22:24:47 +0100

    Refactor render-entrypoint.sh for compatibility and permissions
    Updated the entrypoint script to use sh instead of bash and added logic for handling storage paths and permissions.

--- render-entrypoint.sh
+++ render-entrypoint.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 
-# Optional: fix ownership/permissions if needed
-# Example: if you mount a volume for data
-# chown -R pgadmin:pgadmin /var/lib/pgadmin
+chown pgadmin:root /var/lib/pgadmin
 
-# Call the base image entrypoint to start pgAdmin
-exec /entrypoint.sh "$@"
+# pgadmin will setup initial servers based on the default email of the user
+# the path has the @ symbol replaced with an underscore
+storage_path=/var/lib/pgadmin/storage/$(echo $PGADMIN_DEFAULT_EMAIL | sed 's/@/_/g')
+sudo -u pgadmin mkdir -p -m 00775 $storage_path
+
+# render secret files are owned by root so we need to copy them over with the
+# appropriate user / group / permissions for pgadmin to read the initial servers file
+install -o pgadmin -g root -m 00755 /etc/secrets/servers.json /var/lib/pgadmin/storage/
+# postgres requires the pgpassfile to have specific permissions for security purposes
+install -o pgadmin -g root -m 0600 /etc/secrets/pgpassfile $storage_path
+
+sudo -Eu pgadmin /entrypoint.sh
